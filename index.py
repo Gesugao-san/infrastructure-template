aa47af876cf8551c8795a20d77f61c04eac4419b
Gesugao-san <gesugao.san@gmail.com>
2023-11-02 13:47:15 +0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def _main():
+    """This is docstring.
+    See: https://docs.python.org/3/tutorial/controlflow.html#defining-functions
+    """
+    raise NotImplementedError
+    #
+
+if __name__ == "__main__":
+    print("run")
+    print()
+
+    _main()
+
+    print()
+    print("stop")
+
+SystemExit(0) # exit(0)
